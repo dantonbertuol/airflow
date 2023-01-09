1eeaa6f1527c5619c2e90afe115d593db352fcdd
Danton Bertuol <dantonjb@gmail.com>
2023-01-09 19:46:58 -0300

    Desenvolvidas DAGs de trigger_rules e edge_labels.

--- dags/edge_labels.py
+++ dags/edge_labels.py
@@ -3,11 +3,13 @@ import airflow.utils.dates
 from airflow.operators.bash import BashOperator
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import BranchPythonOperator
+from airflow.utils.trigger_rule import TriggerRule
+from airflow.utils.edgemodifier import Label
 
 # Cria a DAG
 dag = DAG(
-    dag_id="branching_teste",
-    description="DAG de exemplo para o controle de Branching",
+    dag_id="edge_labels",
+    description="DAG de exemplo para edge labels",
     start_date=airflow.utils.dates.days_ago(14),
     schedule_interval=None,
 )
@@ -38,6 +40,12 @@ check_acuracy_op = BranchPythonOperator(
 
 deploy_op = DummyOperator(task_id="deploy_task", dag=dag)
 retrain_op = DummyOperator(task_id="retrain_task", dag=dag)
+# Na tarefa notify_op a regra de acionamento é se nenhuma das tarefas anteriores falhou
+# Se ela foi ignorada ou teve sucesso executa, esse operador de regra pode ser definido por Tarefa
+# Se fosse no trigger_rule padrão, jamais seria executada pq na bifurcação uma será ignorada
+notify_op = DummyOperator(task_id="notify_task", dag=dag, trigger_rule=TriggerRule.NONE_FAILED)
 
-# Ordem de execução das tarefas sendo o último uma bifurcação
-get_acuracy_op >> check_acuracy_op >> [deploy_op, retrain_op]
+# Ordem de execução das tarefas
+get_acuracy_op >> check_acuracy_op
+check_acuracy_op >> Label("ACC >= 90%") >> deploy_op >> notify_op
+check_acuracy_op >> Label("ACC < 90%") >> retrain_op >> notify_op
